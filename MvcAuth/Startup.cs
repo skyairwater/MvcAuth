83090a7b7270b2e67ae1e20e515f329ad41ecfea
skyairwater@gmail.com <skyairwater@gmail.com>
2017-12-24 14:42:27 -0500

    Initial checkin. Visual studio built-in google oauth is working fine.

---
+++ MvcAuth/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAuth.Startup))]
+namespace MvcAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        
+    }
+}
